f42f271c101167845ca1db6903d8b68894013698
Quentin Couland <Coulis1990@gmail.com>
2022-12-05 22:01:36 +0100

    Day 3 part 1

--- third.py
+++ third.py
@@ -10,15 +10,28 @@ def get_misplaced(comp):
     """ Compute the interesction of the two sets """
     return set(comp[0]).intersection(set(comp[1])).pop()
 
+def get_priority(letter):
+    # a to z (1 - 26)
+    ret = ord(letter) - ord('a') + 1
+    
+    if ord(letter) > ord('Z'):
+        return ret
+    
+    # A to Z (27 - 52)
+    else:
+        return ret + ord('z') - ord('A') + 1
+ 
 def sum_priorities(data):
     score = 0
     for comp in data:
-        ret = get_misplaced(comp)
-        breakpoint()
+        score += get_priority(get_misplaced(comp))
+
+    return score
+        
 
 def main():
     data = load_and_format_data()
-    sum_priorities(data)
+    print(f"Summed priorities: {sum_priorities(data)}")
 
 
 if __name__ == "__main__":
