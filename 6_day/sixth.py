db2defe2dbea496d87ab79959b35ac679bded424
Quentin Couland <couland-q@ird222-07-l.etu.polytech.univ-nantes.prive>
2022-12-06 14:01:24 +0100

    :tada: Sixth day

--- 6_day/sixth.py
+++ 6_day/sixth.py
@@ -1,3 +1,27 @@
+def has_diff_letters(buffer):
+    for i, l in enumerate(buffer):
+        for j, l2 in enumerate(buffer):
+            if i != j and l == l2:
+                    return False
+
+    return True
+
+
+def find_marker_index(data, buffer_size=4):
+    buffer = []
+    for i, l in enumerate(data):
+        buffer.append(l)
+
+        if i > buffer_size - 2:
+            if has_diff_letters(buffer):
+                return i + 1
+            else:
+                buffer.pop(0)
+
+    return -1
+
+        
+
 def load_and_format_data():
     """
         Loads the data in two different lists: one for the deck data, one for the moving instructions 
@@ -9,6 +33,8 @@ def load_and_format_data():
 
 def main():
     data = load_and_format_data()
+    print(f"Marker found at {find_marker_index(data)}")
+    print(f"State-of-the-art marker found at {find_marker_index(data, buffer_size=14)}")
 
 if __name__ == "__main__":
     main()
