4bbb43d53a75c0c5f8f6dc0b37da2ce423e72767
Quentin Couland <couland-q@ird222-07-l.etu.polytech.univ-nantes.prive>
2022-12-15 16:54:05 +0100

    :construction: Day 11

---
+++ 11_day/eleventh.py
@@ -0,0 +1,92 @@
+from dataclasses import dataclass, field
+from enum import Enum
+from typing import List
+from webbrowser import Opera
+
+
+class Operator(Enum):
+    ADD = 0,
+    SUB = 1,
+    MUL = 2,
+    DIV = 3,
+    NONE = 4
+
+def operator_to_string(string):
+    if string == "+":
+        return Operator.ADD
+
+    elif string == "-":
+        return Operator.SUB
+
+    elif string == "*":
+        return Operator.MUL
+
+    elif string == "/":
+        return Operator.DIV
+
+    return Operator.NONE
+
+@dataclass
+class Monkey:
+    name: str
+    starting_items: list = field(default_factory=list)
+    values_operator: Operator = Operator.NONE
+    old: int = -1
+    new: int = -1
+    values: list = field(default_factory=list)
+    test_operator: Operator = Operator.NONE
+    value_test = int = -1
+    throw_to: list = field(default_factory=list)
+
+
+def load_and_format_data():
+    """ Loads the data in a list of Monkeys """
+    data = []
+
+    with open("test_data.txt", "r") as input_file:
+        content = input_file.read()
+
+        monkey = None
+
+        for line in content.split("\n"):
+            if not line:
+                continue
+
+            splitted = line.strip().split(" ")
+
+            if splitted[0] == "Monkey":
+                if monkey:
+                    data.append(monkey)
+
+                monkey = Monkey(name=splitted[1])
+
+            elif splitted[0] == "Starting":
+                monkey.starting_items = [int(x) for x in line.split(":")[1].split(",")]
+            
+            elif splitted[0] == "Operation:":
+                operator_splitted = line.split("=")[1].split(" ")
+                
+                try:
+                    monkey.values.append(int(operator_splitted[1]))
+                except ValueError:
+                    monkey.values.append(operator_splitted[1])
+
+
+                monkey.values_operator = operator_to_string(operator_splitted[2])
+
+                try:
+                    monkey.values.append(int(operator_splitted[3]))
+                except ValueError:
+                    monkey.values.append(operator_splitted[3])
+
+            # TODO: Test
+                
+        
+    return data
+
+def main():
+    data = load_and_format_data()
+    breakpoint()
+
+if __name__ == "__main__":
+    main()
